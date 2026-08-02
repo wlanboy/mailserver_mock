c3ef1f2be933eeb18a31dbdefb766f92e73b41b7
wlanboy <wlanboy@githumb.com>
2026-08-02 08:08:07 +0200

    Refactor __init__.py to import server and storage functions and define __all__ for public API

--- src/mailserver_mock/__init__.py
+++ src/mailserver_mock/__init__.py
@@ -1,2 +1,12 @@
-def main() -> None:
-    print("Hello from mailserver-mock!")
+from .server import main, start_mail_server
+from .storage import count_mails, load_flags, load_mail, save_flags, save_mail
+
+__all__ = [
+    "main",
+    "start_mail_server",
+    "count_mails",
+    "load_flags",
+    "load_mail",
+    "save_flags",
+    "save_mail",
+]
